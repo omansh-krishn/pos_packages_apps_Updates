cb702989acc24ef3c7198a162385bf75214dc050
Omansh Krishn <96343317+omansh-krishn@users.noreply.github.com>
2023-08-09 06:44:38 +0530

    adapt endpoints for me
    have a look here -> https://gitlab.com/androidsantoni/ota/pixelos/-/tree/thirteen

--- app/src/main/java/net/pixelos/ota/misc/Constants.java
+++ app/src/main/java/net/pixelos/ota/misc/Constants.java
@@ -29,7 +29,7 @@ public final class Constants {
     static final String PROP_AB_DEVICE = "ro.build.ab_update";
     static final String PROP_DEVICE = "ro.custom.device";
     static final String PROP_BUILD_VERSION = "net.pixelos.version";
-    static final String OTA_URL = "https://raw.githubusercontent.com/PixelOS-AOSP/official_devices/%s/API/updater/%s.json";
+    static final String OTA_URL = "https://gitlab.com/androidsantoni/ota/pixelos/-/raw/%s/API/updater/%s.json";
     static final String MAINTAINER_URL = "https://github.com/%s";
     static final String DOWNLOAD_WEBPAGE_URL = "https://pixelos.net/download/%s";
     static final String EXPORT_PATH = "PixelOS-Updates/";
